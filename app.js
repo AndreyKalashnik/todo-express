dcdddc26069f4320fdeab40193a5074b649ec4da
andrii.kalashnik <kalashnik.a.p@gmail.com>
2025-02-15 17:21:34 +0100

    added a middleware logger

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    res.send('Hello world');
+});
+
+app.use((req, res, next) => {
+    console.log(`Request Type: ${req.method}`);
+    console.log(`Request URL: ${req.url}`);
+    next();
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
+    console.log(`Server is running on http://localhost:${port}`);
 });
