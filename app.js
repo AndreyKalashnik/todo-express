de5d4300eebb673759623ed8955151b78ed8e87a
andrii.kalashnik <kalashnik.a.p@gmail.com>
2025-02-15 19:31:21 +0100

    added CRUD for todos

--- app.js
+++ app.js
@@ -1,6 +1,41 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
+
+let todos = [
+    {id: 1, task: 'Learn Node.js'},
+]
+
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+app.post('/todos', (req, res) => {
+    const {task} = req.body;
+    const newTodo = {id: todos.length + 1, task: task};
+
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+});
+
+app.put('/todos/:id', (req, res) => {
+    const {id} = req.params;
+    const {task} = req.body;
+    const updatedTodo = {id, task};
+    
+    todos = todos.map(todo => (todo.id === parseInt(id)) ? updatedTodo : todo);
+    res.json(todos.find(todo => todo.id === parseInt(id)));
+});
+
+app.delete('/todos/:id', (req, res) => {
+    const {id} = req.params;
+
+    todos = todos.filter(todo => todo.id !== parseInt(id));
+    res.status(204).send();
+});
 
 app.get('/', (req, res) => {
     res.send('Hello world');
